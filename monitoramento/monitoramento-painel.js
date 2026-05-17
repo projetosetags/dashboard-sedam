304fd72a4405f8c243f5035624c56d77f81a1629
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-05-17 17:13:52 -0300

    Update monitoramento-painel.js

--- monitoramento/monitoramento-painel.js
+++ monitoramento/monitoramento-painel.js
@@ -57,8 +57,27 @@ let riscoBaixo=0
 
 let beneficios=0
 
-;(data||[]).forEach(i=>{
+data=(data||[]).sort((a,b)=>{
 
+let sa=String(a.subitem||'0.0').split('.').map(Number)
+let sb=String(b.subitem||'0.0').split('.').map(Number)
+
+for(let i=0;i<Math.max(sa.length,sb.length);i++){
+
+let va=sa[i]||0
+let vb=sb[i]||0
+
+if(va!==vb){
+return va-vb
+}
+
+}
+
+return 0
+
+})
+
+;(data||[]).forEach(i=>{
 let percentual=Number(i.percentual||0)
 
 beneficios+=percentual
